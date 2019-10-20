4a1074ca92afd70dc6bc1bb3d39492dae48984c1
sadimfinity <santi-skate@hotmail.com>
2019-10-19 19:11:04 -0500

    Initializing the basic express server

---
+++ config.js
@@ -0,0 +1,32 @@
+/**
+ * Server configuration
+ */
+const port = process.env.DEV ? 5000 : 8080;
+const morganMode = process.env.Dev ? 'dev' : 'tiny';
+
+/**
+ * MongoDB credentials
+ */
+const mongoUser = 'admin';
+const mongoPassword = 'root';
+const mongoHost = 'localhost';
+const mongoPort = '27017';
+const mongoDatabase = 'local';
+const mongoOpt = {
+    user: 'admin',                            // DB Username
+    pass: 'root',                             // DB 
+    dbName: 'local'
+  };
+
+/**
+ * MongoDB URL. Can be a local or a remote one.
+ */
+//const mongoDB = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}`;
+const mongoDB = 'mongodb://localhost:27017/local'
+
+module.exports = {
+    port,
+    morganMode,
+    mongoDB,
+    mongoOpt
+};
